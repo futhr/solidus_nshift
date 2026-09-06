80e9e361c33602dcf2c20df7663cc2b9e248203f
Tobias Bohwalli <hi@futhr.io>
2026-09-06 17:31:20 +0200

    fix: load admin integration only when Solidus backend is installed

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,19 @@
 SolidusNshift::Engine.routes.draw do
   resources :rate_selections, only: :update
 
-  namespace :admin do
-    resources :connections
-    resources :fulfillments, only: %i[index show] do
-      member do
-        post :book
-        post :reconcile
-        post :sync_tracking
-        post :cancel
-        post :refresh_documents
+  if SolidusSupport.backend_available?
+    namespace :admin do
+      resources :connections
+      resources :fulfillments, only: %i[index show] do
+        member do
+          post :book
+          post :reconcile
+          post :sync_tracking
+          post :cancel
+          post :refresh_documents
+        end
       end
+      resources :documents, only: :show
     end
-    resources :documents, only: :show
   end
 end
